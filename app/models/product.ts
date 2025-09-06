10a7c3dcb2843b4c8a30502afc8fa342fd59002c
zgeniusecoders <zgeniusecoders@gmail.com>
2025-09-06 14:20:43 +0100

    feat: ajout de la relation category et orders dans le modele product.ts

--- app/models/product.ts
+++ app/models/product.ts
@@ -1,7 +1,9 @@
 import { DateTime } from 'luxon'
-import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
 import Company from '#models/company'
 import * as relations from '@adonisjs/lucid/types/relations'
+import Order from '#models/order'
+import Category from '#models/category'
 
 export default class Product extends BaseModel {
   @column({ isPrimary: true })
@@ -25,8 +27,14 @@ export default class Product extends BaseModel {
   @column()
   declare photo: string
 
+  @belongsTo(() => Category)
+  declare category: relations.BelongsTo<typeof Category>
+
   @belongsTo(() => Company)
   declare company: relations.BelongsTo<typeof Company>
+
+  @hasMany(() => Order)
+  declare orders: relations.HasMany<typeof Order>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime

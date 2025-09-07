bc4f28b6a27cd7775e51e9d2851166f8f9e1d08b
zgeniusecoders <zgeniusecoders@gmail.com>
2025-09-07 09:23:10 +0100

    feat: ajout regle de validation pour ajouter ou modifer un produit

---
+++ app/validators/product.ts
@@ -0,0 +1,31 @@
+import vine from '@vinejs/vine'
+
+export const addProductValidator = vine.compile(
+  vine.object({
+    name: vine.string().escape(),
+    description: vine.string().escape().minLength(10).maxLength(255),
+    price: vine.number(),
+    categoryId: vine.number().exists({ table: 'categories', column: 'id' }),
+    companyId: vine.number().exists({ table: 'companies', column: 'id' }),
+    photo: vine.file({
+      extnames: ['jpg', 'png', 'jpeg'],
+      size: '10mb',
+    }),
+  })
+)
+
+export const updateProductValidator = vine.compile(
+  vine.object({
+    name: vine.string().escape().optional(),
+    description: vine.string().escape().minLength(10).maxLength(255).optional(),
+    price: vine.number().optional(),
+    categoryId: vine.number().exists({ table: 'categories', column: 'id' }).optional(),
+    companyId: vine.number().exists({ table: 'companies', column: 'id' }).optional(),
+    photo: vine
+      .file({
+        extnames: ['jpg', 'png', 'jpeg'],
+        size: '10mb',
+      })
+      .optional(),
+  })
+)

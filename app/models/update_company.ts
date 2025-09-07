56868f5106cc4495f981f641e7f06bcc3c63b585
zgeniusecoders <zgeniusecoders@gmail.com>
2025-09-07 08:40:37 +0100

    feat:création du modele update_company.ts

---
+++ app/models/update_company.ts
@@ -0,0 +1,7 @@
+export interface UpdateCompany {
+  name?: string | undefined
+  ownerId?: number | undefined
+  phoneNumber?: string | undefined
+  idNat?: string | undefined
+  rccm?: string | undefined
+}

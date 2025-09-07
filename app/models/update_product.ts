e84be8c12dadc5b856ed5115a063628b3b9e745a
zgeniusecoders <zgeniusecoders@gmail.com>
2025-09-07 09:21:11 +0100

    feat: création du modele update_product.ts

---
+++ app/models/update_product.ts
@@ -0,0 +1,8 @@
+export interface UpdateProduct {
+  name?: string | undefined
+  description?: string | undefined
+  price?: number | undefined
+  categoryId?: number | undefined
+  photo?: string | undefined
+  companyId?: number | undefined
+}

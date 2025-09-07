02dec5a2e7468c8b7dc162b6386a40bc91120f97
zgeniusecoders <zgeniusecoders@gmail.com>
2025-09-07 09:55:12 +0100

    feat: ajout regle de validation pour  ajouter et mettre a jour une commande

---
+++ app/validators/order.ts
@@ -0,0 +1,17 @@
+import vine from '@vinejs/vine'
+import { OrderStatusEnum } from '../enum/OrderStatusEnum.js'
+
+export const addOrderValidator = vine.compile(
+  vine.object({
+    productId: vine.number().exists({ table: 'products', column: 'id' }),
+    userId: vine.number().exists({ table: 'users', column: 'id' }),
+    price: vine.number(),
+    quantity: vine.number(),
+  })
+)
+
+export const updateOrderValidator = vine.compile(
+  vine.object({
+    status: vine.string().in(Object.values(OrderStatusEnum)),
+  })
+)

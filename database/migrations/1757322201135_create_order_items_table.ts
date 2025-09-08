5cc57ec7d74687a1fd1fff65e10839eea7b8935f
zgeniusecoders <zgeniusecoders@gmail.com>
2025-09-08 10:07:39 +0100

    feat: créatin de la table order_items

---
+++ database/migrations/1757322201135_create_order_items_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'order_items'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('order_id').unsigned().references('orders.id').onDelete('cascade')
+      table.integer('product_id').unsigned().references('products.id').onDelete('cascade')
+      table.integer('quantity')
+      table.double('price')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}

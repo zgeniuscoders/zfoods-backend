1ffc5e9c0c4bec643d862097936794096722501a
zgeniusecoders <zgeniusecoders@gmail.com>
2025-09-08 10:00:49 +0100

    refactor: suppression des attribut product_id et  quantity dans le modele order.ts

--- app/models/order.ts
+++ app/models/order.ts
@@ -1,6 +1,5 @@
 import { DateTime } from 'luxon'
 import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
-import Product from '#models/product'
 import * as relations from '@adonisjs/lucid/types/relations'
 import User from '#models/user'
 
@@ -9,22 +8,13 @@ export default class Order extends BaseModel {
   declare id: number
 
   @column()
-  declare productId: number
-
-  @column()
   declare userId: number
 
   @column()
   declare status: string
 
   @column()
-  declare quantity: number
-
-  @column()
   declare price: number
-
-  @belongsTo(() => Product)
-  declare product: relations.BelongsTo<typeof Product>
 
   @belongsTo(() => User)
   declare user: relations.BelongsTo<typeof User>

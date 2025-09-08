0d6acc6bcd54cbec390f64b84168407da80562c5
zgeniusecoders <zgeniusecoders@gmail.com>
2025-09-08 12:05:38 +0100

    refactor: renomage du fichier add_category.ts en category.ts

---
+++ app/validators/category.ts
@@ -0,0 +1,23 @@
+import vine from '@vinejs/vine'
+
+export const addCategoryValidator = vine.compile(
+  vine.object({
+    name: vine.string(),
+    photo: vine.file({
+      extnames: ['jpg', 'png', 'jpeg'],
+      size: '10mb',
+    }),
+  })
+)
+
+export const updateCategoryValidator = vine.compile(
+  vine.object({
+    name: vine.string().optional(),
+    photo: vine
+      .file({
+        extnames: ['jpg', 'png', 'jpeg'],
+        size: '10mb',
+      })
+      .optional(),
+  })
+)

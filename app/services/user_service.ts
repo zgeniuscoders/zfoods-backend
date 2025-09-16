b443aa265a2dac8d5c7d964671e436cc87ecb6bd
zgeniusecoders <zgeniusecoders@gmail.com>
2025-09-16 13:34:55 +0100

    feat(UserService): creation du user_service

--- app/services/user_service.ts
+++ app/services/user_service.ts
@@ -1,3 +1,21 @@
+import { UpdateUser } from '#models/update_user'
+import User from '#models/user'
+
 export class UserService {
-  // Your code here
+  async getUsers(page: number, perPage: number) {
+    return User.query().paginate(page, perPage)
+  }
+
+  async getUser(id: number) {
+    return User.findOrFail(id)
+  }
+
+  async updateUser(id: number, data: UpdateUser) {
+    await User.query().where({ id: id }).update(data)
+  }
+
+  async deleteUser(id: number) {
+    const user = await User.findOrFail(id)
+    await user.delete()
+  }
 }
